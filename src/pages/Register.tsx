d34e3231efcedf5aee5bc31ac26a700a1d8150e7
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-01-24 17:31:31 +0700

    Feat: Styling Register Pages

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -11,19 +11,27 @@ function Register() {
       <div className="mx-auto bg-white max-w-md rounded-3xl p-5 my-10">
         <h1 className="text-3xl text-center mb-10">Register</h1>
         <div className="mb-5">
-          <p>Email/Username</p>
-          <InputForm />
+          <p>Username</p>
+          <InputForm placeholder="Username" />
+        </div>
+        <div className="mb-5">
+          <p>Full Name</p>
+          <InputForm placeholder="Full Name" />
+        </div>
+        <div className="mb-5">
+          <p>Email</p>
+          <InputForm placeholder="Email" />
         </div>
         <div className="mb-5">
           <p>Password</p>
-          <InputForm />
+          <InputForm placeholder="Password" type="password" />
         </div>
-        <div className="flex flex-col gap-3 my-5">
+        <div className="mb-5">
+          <p>Confirm Password</p>
+          <InputForm placeholder="Confirm Password" type="password" />
+        </div>
+        <div className="flex flex-col gap-3 my-10">
           <Btn label="Register" />
-          <p className="text-center">Or</p>
-          <Link to="/register">
-            <Btns label="Register" />
-          </Link>
         </div>
       </div>
     </LayoutForm>
