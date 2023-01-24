87c34c8bc925db763808ad0cf7c22e70396cd8e7
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-01-24 17:23:56 +0700

    Feat: Styling login pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { LayoutForm } from "../components/Layout";
+import { InputForm } from "../components/Input";
+import { RememberMe } from "../components/Checkbox";
+import { Btn, Btns } from "../components/Button";
 
 function Login() {
   return (
     <LayoutForm>
-      <div className="h-screen"></div>
+      <div className="mx-auto bg-white max-w-md rounded-3xl p-5 my-10">
+        <h1 className="text-3xl text-center mb-10">Login</h1>
+        <div className="mb-5">
+          <p>Email/Username</p>
+          <InputForm placeholder="Email/Username" />
+        </div>
+        <div className="mb-5">
+          <p>Password</p>
+          <InputForm placeholder="Password" type="password" />
+        </div>
+        <RememberMe />
+        <div className="flex flex-col gap-3 my-5">
+          <Btn label="Login" />
+          <p className="text-center">Or</p>
+          <Link to="/register">
+            <Btns label="Register" />
+          </Link>
+        </div>
+      </div>
     </LayoutForm>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { LayoutForm } from "../components/Layout";
+import { InputForm } from "../components/Input";
+import { Btn, Btns } from "../components/Button";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <LayoutForm>
+      <div className="mx-auto bg-white max-w-md rounded-3xl p-5 my-10">
+        <h1 className="text-3xl text-center mb-10">Register</h1>
+        <div className="mb-5">
+          <p>Email/Username</p>
+          <InputForm />
+        </div>
+        <div className="mb-5">
+          <p>Password</p>
+          <InputForm />
+        </div>
+        <div className="flex flex-col gap-3 my-5">
+          <Btn label="Register" />
+          <p className="text-center">Or</p>
+          <Link to="/register">
+            <Btns label="Register" />
+          </Link>
+        </div>
+      </div>
+    </LayoutForm>
+  );
 }
 
-export default Register
+export default Register;
