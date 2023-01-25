838c727ab490e844d8d4b4a5afdc12bba4a3e5f0
Irfan Fitriandi <irfanfitriandi@gmail.com>
2023-01-25 13:27:13 +0700

    Feat: Register using openAPI

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,27 +2,40 @@ import { FC, ButtonHTMLAttributes } from "react";
 
 import { Button } from "react-daisyui";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   icon?: JSX.Element;
   className?: string;
   disabled?: boolean;
 }
 
-export function Btn({ label, className, disabled, icon }: ButtonProps) {
+export function Btn({
+  label,
+  className,
+  disabled,
+  icon,
+}: ButtonProps) {
   return (
     <Button
       className={`bg-btn text-white hover:bg-btnh border-none rounded-full w-full ${className}`}
+      disabled={disabled}
     >
       {label} {icon}
     </Button>
   );
 }
 
-export function Btns({ label, className, disabled, icon }: ButtonProps) {
+export function Btns({
+  label,
+  className,
+  disabled,
+  icon,
+}: ButtonProps) {
   return (
     <Button
       className={`bg-btns text-white hover:bg-btnsh border-none rounded-full w-full ${className}`}
+      disabled={disabled}
     >
       {label} {icon}
     </Button>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,38 +1,118 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 import { LayoutForm } from "../components/Layout";
 import { InputForm } from "../components/Input";
-import { Btn, Btns } from "../components/Button";
+import { Btn } from "../components/Button";
 
 function Register() {
+  const [formRegister, setFormRegister] = useState({
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [disabled, setDisabled] = useState(true);
+  const [rePass, setRePass] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e: any) => {
+    setFormRegister({
+      ...formRegister,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (
+      formRegister.name === "" ||
+      formRegister.username === "" ||
+      formRegister.email === "" ||
+      formRegister.password === ""
+    ) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [formRegister]);
+
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    if (rePass !== formRegister.password) {
+      alert("Password not match");
+      return;
+    }
+    axios
+      .post(
+        "https://virtserver.swaggerhub.com/audizzy/ecommerce/1.0.0/register",
+        formRegister
+      )
+      .then((res) => {
+        console.log(res);
+        alert("Success signup");
+        navigate("/login");
+      })
+      .catch((err) => {
+        alert(err.toString());
+      });
+  };
+
   return (
     <LayoutForm>
       <div className="mx-auto bg-white max-w-md rounded-3xl p-5 my-10">
-        <h1 className="text-3xl text-center mb-10">Register</h1>
-        <div className="mb-5">
-          <p>Username</p>
-          <InputForm placeholder="Username" />
-        </div>
-        <div className="mb-5">
-          <p>Full Name</p>
-          <InputForm placeholder="Full Name" />
-        </div>
-        <div className="mb-5">
-          <p>Email</p>
-          <InputForm placeholder="Email" />
-        </div>
-        <div className="mb-5">
-          <p>Password</p>
-          <InputForm placeholder="Password" type="password" />
-        </div>
-        <div className="mb-5">
-          <p>Confirm Password</p>
-          <InputForm placeholder="Confirm Password" type="password" />
-        </div>
-        <div className="flex flex-col gap-3 my-10">
-          <Btn label="Register" />
-        </div>
+        <h1 className="text-3xl text-center mb-10">
+          Register
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <InputForm
+            id="username"
+            title="Username"
+            placeholder="Username"
+            type="text"
+            onChange={handleChange}
+            value={formRegister.username}
+          />
+          <InputForm
+            id="name"
+            title="Full Name"
+            placeholder="Full Name"
+            type="text"
+            onChange={handleChange}
+            value={formRegister.name}
+          />
+          <InputForm
+            id="email"
+            title="Email"
+            placeholder="Email"
+            type="email"
+            onChange={handleChange}
+            value={formRegister.email}
+          />
+          <InputForm
+            id="password"
+            title="Password"
+            placeholder="Password"
+            type="password"
+            onChange={handleChange}
+            value={formRegister.password}
+          />
+          <InputForm
+            id="password"
+            title="Confirm Password"
+            placeholder="Confirm Password"
+            type="password"
+            onChange={(e) => setRePass(e.target.value)}
+            value={rePass}
+          />
+          <Btn
+            disabled={disabled}
+            className="my-10"
+            label="Register"
+          />
+        </form>
       </div>
     </LayoutForm>
   );
